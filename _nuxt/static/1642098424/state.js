25280b08ad94f3b2842ccca082621693822d8ce1
Dirk Baumann <dirkbaumann@iMac.fritz.box>
2022-01-13 20:09:32 +0100

    Updates

--- _nuxt/static/1642098424/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstoryblok1\u002F_nuxt\u002Fstatic\u002F1642098424",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fstoryblok1\u002F",assetsPath:"\u002Fstoryblok1\u002F_nuxt\u002F",cdnURL:a}}}}(null));
